135524b4449278507baf02d776dddd96ca643366
ImDavidCO <davorgiiii@gmail.com>
2023-09-19 02:32:31 -0500

    api request added, input pending

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './navbar.css';
 import {BiSearchAlt} from 'react-icons/bi';
 
+
 const Navbar = () => {
-  return (
+    var searchinput = useRef();
+    var input = JSON.stringify(searchinput);
+    const request = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Cookie", "trackingId=rB8XfGUI8yKyKQr8CmqdAg==");
+    
+        var raw = JSON.stringify({
+        "query": input
+        });
+    
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+    
+        fetch("https://torre.ai/api/entities/_searchStream", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+    return (
     <nav>
-        <searchInput/>
-      <a href='#about'><BiSearchAlt/></a>
-      <input className='' placeholder='Search' type='text' name='name' autoComplete='name' required/>
+      <a href='#app' onClick={request}><BiSearchAlt/></a>
+      <input ref={searchinput} placeholder='Search' type='text' name='name' autoComplete='name' required/>
     </nav>
   )
+
 }
 
 export default Navbar;
